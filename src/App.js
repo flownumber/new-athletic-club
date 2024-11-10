e225aad5f7546513739161ccc939fcb7e0082e12
flownumber <flownumber458@gmail.com>
2024-11-10 15:25:39 +0100

    daije

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import React, { useState, useEffect } from 'react';
 import usersData from './data/users.json';
+import HomePage from './pages/HomePage';
+import './App.css';
 
 function App() {
   const [users, setUsers] = useState(usersData);
 
   return (
-    <Router>
-      <div className="App" style={{ backgroundColor: 'black', color: 'gold' }}>
-        <Routes>
-          <Route path="/" element={<HomePage users={users} setUsers={setUsers} />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App" style={{ backgroundColor: 'black', color: 'gold' }}>
+      <HomePage users={users} setUsers={setUsers} />
+    </div>
   );
 }
 
